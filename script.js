a0c86f1c8b7331451fcb8aaa141f2ab01755918c
matteodedo <141860088+matteodedo@users.noreply.github.com>
2023-08-20 18:12:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,59 @@ const onProgress = (event) => {
   }
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
+//nascondo immagini
+document.getElementById("HotspotAnnotation").style.visibility = "hidden";
+
+
+
+function hideElem() {
+    document.getElementById("piazza").style.visibility = "hidden"; 
+    document.getElementById("cima").style.visibility = "hidden";
+    document.getElementById("yale").style.visibility = "hidden"; 
+    document.getElementById("campon").style.visibility = "hidden"; 
+}
+
+function piazza() {
+    document.getElementById("piazza").style.visibility = "visible";
+    document.getElementById("cima").style.visibility = "hidden";
+    document.getElementById("yale").style.visibility = "hidden"; 
+    document.getElementById("campon").style.visibility = "hidden";
+}
+function cimaloreto() {
+    document.getElementById("piazza").style.visibility = "hidden"; 
+    document.getElementById("cima").style.visibility = "visible";
+    document.getElementById("yale").style.visibility = "hidden"; 
+    document.getElementById("campon").style.visibility = "hidden";
+}
+function yale() {
+    document.getElementById("piazza").style.visibility = "hidden"; 
+    document.getElementById("cima").style.visibility = "hidden";
+    document.getElementById("yale").style.visibility = "visible"; 
+    document.getElementById("campon").style.visibility = "hidden";
+}
+function campon() {
+    document.getElementById("piazza").style.visibility = "hidden"; 
+    document.getElementById("cima").style.visibility = "hidden";
+    document.getElementById("yale").style.visibility = "hidden"; 
+    document.getElementById("campon").style.visibility = "visible";
+}
+//-----------------------------------------------------------------------
+function gal_piazza() {
+    document.getElementById("gal_piazza").style.visibility = "visible"; 
+}
+function gal_cima() {
+    document.getElementById("gal_cima").style.visibility = "visible"; 
+}
+function gal_yale() {
+    document.getElementById("gal_yale").style.visibility = "visible"; 
+}
+function gal_campon() {
+    document.getElementById("gal_campon").style.visibility = "visible"; 
+}
+function nascondi() {
+    document.getElementById("gal_piazza").style.visibility = "hidden"; 
+    document.getElementById("gal_cima").style.visibility = "hidden";
+    document.getElementById("gal_yale").style.visibility = "hidden";
+    document.getElementById("gal_campon").style.visibility = "hidden";
+}
+
